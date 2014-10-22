51a5dd4ee26fb0aa3616b68800097f414e6c099a
ken restivo <ken@restivo.org>
2014-10-22 12:12:17 -0700

    Some scratch

--- test/bufferplot/core_test.clj
+++ test/bufferplot/core_test.clj
@@ -2,6 +2,32 @@
   (:require [clojure.test :refer :all]
             [bufferplot.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(comment
+
+  (plot-buffer "/mnt/sdcard/tmp/marco-buffer.log")
+
+  (plot-buffer "/mnt/sdcard/tmp/parade.log")
+
+  (plot-buffer "/mnt/sdcard/tmp/randomshow-buf.log")
+
+
+  (plot-buffer "/mnt/sdcard/tmp/blog2")
+
+  (plot-buffer "/mnt/sdcard/tmp/ices2-test.log")
+
+  (plot-buffer "/mnt/sdcard/tmp/ices-bamfic.log")
+
+
+  (plot-buffer "/mnt/sdcard/tmp/with-441.log")
+
+  (plot-buffer "/mnt/sdcard/tmp/denny.log")
+  (save @plot "/home/cust/knyo/logs/denny.png" :width 1280 :height 768)
+  (save-pdf @plot "/home/cust/knyo/logs/denny.pdf" :width 1280 :height 768)
+
+
+  (plot-buffer "/mnt/sdcard/tmp/reggae.log")
+  (save @plot "/home/cust/knyo/logs/reggae.png" :width 1280 :height 768)
+  (save-pdf @plot "/mnt/sdcard/tmp/reggae.pdf" :width 1280 :height 768)
+  
+  )
